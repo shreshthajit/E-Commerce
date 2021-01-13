29bc2d7801f59b068f3dbe9a4cabb4abfe0c6631
Shreshthajit Das <43321488+shreshthajit@users.noreply.github.com>
2021-01-13 11:59:57 +0600

    Update mail.php

--- mail.php
+++ mail.php
@@ -21,16 +21,16 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'mobile3free@gmail.com';                     // SMTP username
-    $mail->Password   = 'ismail61';                               // SMTP password
+    $mail->Username   = 'your email';                     // SMTP username
+    $mail->Password   = 'your passward';                               // SMTP password
     $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('mobile3free@gmail.com', 'Mailer');
-    $mail->addAddress('ismailhosen601@gmail.com', 'Joe User');     // Add a recipient
+    $mail->setFrom('your email', 'Mailer');
+    $mail->addAddress('receiver email', 'Joe User');     // Add a recipient
     //$mail->addAddress('ellen@example.com');               // Name is optional
-    $mail->addReplyTo('mobile3free@gmail.com.com', 'Information');
+    $mail->addReplyTo('your email', 'Information');
     //$mail->addCC('cc@example.com');
     //$mail->addBCC('bcc@example.com');
 
